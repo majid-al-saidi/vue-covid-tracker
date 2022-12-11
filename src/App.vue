9ab38daeb4b77e7053c9370d667612560567b330
majid-al-saidi <majid-al-saidi@outlook.com>
2022-12-11 07:24:06 +0400

    prepare app and add header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header />
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+<script>
+import Header from '@/components/header.vue'
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default{
+  components:{
+    Header,
+  },
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
