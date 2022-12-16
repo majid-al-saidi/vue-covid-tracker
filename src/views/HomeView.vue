663fae5c6a1c32a5cd661d857886fdf0676cecb9
majid-al-saidi <majid-al-saidi@outlook.com>
2022-12-16 22:39:36 +0400

    fetch Covid Data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,8 +9,17 @@
 
 export default {
   name: 'HomeView',
-  components: {
-    
-  }
+  components: {},
+  methods: {
+    async fetchCovidData(){
+      const results = await fetch('https://api.covid19api.com/summary');
+      const data = await results.json();
+      return data;
+    }
+  },
+  async created() {
+    const covidData = await this.fetchCovidData();
+    console.log(covidData); 
+  },
 }
 </script>
