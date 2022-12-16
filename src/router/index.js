d94b45386164d23e0291f7f7a00841e1d726f6f3
majid-al-saidi <majid-al-saidi@outlook.com>
2022-12-16 22:30:37 +0400

    add new page and edit the route

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProjectView from '../views/ProjectView'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  }
+  },
+  {
+    path: '/project',
+    name: 'project',
+    component: ProjectView
+  },
 ]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+      Covid Tracker | project Page
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'ProjectView',
+    components: {
+      
+    }
+  }
+  </script>
+  
