8edee23531cfe25b1c7211cfa869a48ec421139f
majid-al-saidi <majid-al-saidi@outlook.com>
2022-12-17 20:49:15 +0400

    Complete Project page

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,7 +1,72 @@
 <template>
-    <div class="home">
-      Covid Tracker | project Page
+    <div>
+    <div class="bg-white rounded-lg shadow-xl py-8 ">
+        <h4 class="text-4xl font-bold text-gray-800 tracking-widest uppercase text-center">Project Vue Covid Tracker</h4>
+        <p class="text-center text-gray-600 text-sm mt-2">Here are some of the frequently asked questions</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-12 px-2 xl:px-12 mt-4">
+            <div class="flex space-x-8 mt-8">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
+                    </svg>
+                </div>
+                <div>
+                    <h4 class="text-xl font-bold text-gray-700">What this app do?</h4>
+                    <p class="text-gray-600 my-2">Get the latest information from the WHO about coronavirus Allowing users to view covid details global or by selecting a country.</p>
+                    <a href="/" class="text-blue-600 hover:text-blue-800 hover:underline capitalize" title="Read More">Check Home</a>
+                </div>
+            </div>
+            
+            <div class="flex space-x-8 mt-8">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                    </svg>
+                </div>
+                <div>
+                    <h4 class="text-xl font-bold text-gray-700">What Technology?</h4>
+                    <p class="text-gray-600 my-2">
+                      Vue.js and Tailwind</p>
+                   
+                </div>
+            </div>
+
+            
+
+            <div class="flex space-x-8 mt-8">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"></path>
+                    </svg>
+                </div>
+                <div>
+                    <h4 class="text-xl font-bold text-gray-700">totorial?</h4>
+                    <p class="text-gray-600 my-2">This project from thie totorial:</p>
+                    <iframe width="460" height="315" src="https://www.youtube.com/embed/m-MAIpnH9ag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    
+                </div>
+            </div>
+            <div class="flex space-x-8 mt-8">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path>
+                        </svg>
+                </div>
+                <div>
+                    <h4 class="text-xl font-bold text-gray-700">API?</h4>
+                    <p class="text-gray-600 my-2">This project uses the <a href="https://covid19api.com/" class="font-bold">covid19api.com</a> by <a href="https://twitter.com/ksredelinghuys" class="font-bold">Kyle Redelinghuys</a>
+
+                    </p>
+                    <a href="https://covid19api.com/" class="text-blue-600 hover:text-blue-800 hover:underline capitalize" title="Read More">covid19api.com</a> <tr/>
+                    <a href="https://twitter.com/ksredelinghuys" class="text-blue-600 hover:text-blue-800 hover:underline capitalize" title="Read More">Kyle Redelinghuys</a>
+                </div>
+            </div>
+            
+        </div>
     </div>
+</div>
   </template>
   
   <script>
